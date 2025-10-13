d96a7c02000f52e3e01b0a6090554d06f86cc24f
Marcos Vinícius <105286480+marcos-so@users.noreply.github.com>
2025-10-13 09:20:59 -0300

    feat: atividade 1

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    userId = 101
+    nome = "Marcos"
+    email = "marcos.souza@example.com"
+    ativo = True
+    tags = ["admin", "backend", "api"]
+
+    print("ID:", userId)
+    print("Nome:", nome)
+    print("Email:", email)
+    print("Ativo:", ativo)
+    print("Tags:", tags)
+
+
+if __name__ == "__main__":
+    main()
